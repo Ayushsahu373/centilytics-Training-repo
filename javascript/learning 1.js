e2426465d778387d53c02ec8b86c256524f87fc2
Ayush Sahu <as5777427@gmail.com>
2025-02-28 15:59:42 +0530

    js

---
+++ javascript/learning 1.js
@@ -0,0 +1,40 @@
+console.log("ayush");
+
+let nam = 'ayush'; 
+let age = 20;
+let isMale = true;
+let a = undefined; 
+let b = null;
+let c = 1234567890123456789012345678901234567890n;
+let d = Symbol('ayush'); 
+let e = { name: 'ayush', age: 20 }; // Object
+
+console.log(typeof a); 
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+isMale='ayush';
+console.log(typeof isMale);
+
+
+//for const
+console.log("ayush");
+
+const nams = 'ayush'; 
+const ages = 20;
+const isMales = true;
+const as = undefined; 
+const bs = null;
+const cs = 1234567890123456789012345678901234567890n;
+const ds = Symbol('ayush'); 
+const es = { name: 'ayush', age: 20 }; // Object
+
+console.log(typeof as); 
+console.log(typeof bs);
+console.log(typeof cs);
+console.log(typeof ds);
+console.log(typeof es);
+
+// isMales = 'ayush'; // ❌ This will throw an error (Assignment to constant variable)
+console.log(typeof isMales);
