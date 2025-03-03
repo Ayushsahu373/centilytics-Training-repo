323b10ce2f18287092b16d877a0ba48b018e79fa
Ayush Sahu <as5777427@gmail.com>
2025-03-03 12:17:55 +0530

    array in js

---
+++ javascript/3.3.25.js
@@ -0,0 +1,75 @@
+
+console.log( new Array())
+console.log(new Array("element1"))
+console.log(new Array("element1", "element2"))
+console.log(new Array("element1", "element2", "elementN"))
+console.log(new Array(5))
+
+const fruits = ["Apple", "Banana"];
+
+console.log(fruits.length); // 2
+console.log(fruits[0]); // "Apple"
+const arrayEmpty = new Array(2);
+
+console.log(arrayEmpty.length); // 2
+console.log(arrayEmpty[0]); // undefined; actually, it is an empty slot
+console.log(0 in arrayEmpty); // false
+console.log(1 in arrayEmpty); // false
+
+const arrayOfOne = new Array("2"); // Not the number 2 but the string "2"
+
+console.log(arrayOfOne.length); // 1
+console.log(arrayOfOne[0]); // "2"
+
+const fruits2 = new Array("Apple", "Banana");
+
+console.log(fruits2.length); // 2
+console.log(fruits2[0]); // "Apple"
+  
+
+
+
+// at 
+console.log("at method example");
+
+
+// Our array with items
+const cart = ["apple", "banana", "pear"];
+
+// A function which returns the last item of a given array
+function returnLast(arr) {
+  return arr.at(-1);
+}
+
+// Get the last item of our array 'cart'
+const item1 = returnLast(cart);
+console.log(item1); // 'pear'
+
+// Add an item to our 'cart' array
+cart.push("orange");
+const item2 = returnLast(cart);
+console.log(item2); // 'orange'
+
+// Our array with items
+const colors = ["red", "green", "blue"];
+
+// Using length property
+const lengthWay = colors[colors.length - 2];
+console.log(lengthWay); // 'green'
+
+// Using slice() method. Note an array is returned
+const sliceWay = colors.slice(-2, -1);
+console.log(sliceWay[0]); // 'green'
+
+// Using at() method
+const atWay = colors.at(-2);
+console.log(atWay); // 'green'
+
+const arrayLike = {
+    length: 2,
+    0: "a",
+    1: "b",
+    2: "c", // ignored by at() since length is 2
+  };
+  console.log(Array.prototype.at.call(arrayLike, 0)); // "a"
+  console.log(Array.prototype.at.call(arrayLike, 2)); // undefined
