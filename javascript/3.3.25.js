f802a7507f93d7c955c87d03afb410e3984607d0
Ayush Sahu <as5777427@gmail.com>
2025-03-03 12:39:56 +0530

    bulb project

--- javascript/3.3.25.js
+++ javascript/3.3.25.js
@@ -71,5 +71,6 @@ const arrayLike = {
     1: "b",
     2: "c", // ignored by at() since length is 2
   };
+  console.log(arrayLike);
   console.log(Array.prototype.at.call(arrayLike, 0)); // "a"
   console.log(Array.prototype.at.call(arrayLike, 2)); // undefined
