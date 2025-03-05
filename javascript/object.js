7f5899f101389fd0ce2fd29a3fbba2ce6033090a
Ayush Sahu <as5777427@gmail.com>
2025-03-05 18:20:15 +0530

    obj,nested obj,array inside obj etc

---
+++ javascript/object.js
@@ -0,0 +1,14 @@
+var obj={
+    "id":589,
+    "name":"Ayush",
+    "email":"ayush.sahu@centilytics.com",
+    "colleague":["anurag","aavi","anshika"],
+    "address":{
+        "city":"Bhopal",
+        "state":"MP",
+        "country":"India"
+    },
+    "colleague-profile":{"anurag":"product manager","aavi":"backend","anshika":"frontend"},
+}
+console.log(obj);
+console.log(JSON.stringify(obj))
